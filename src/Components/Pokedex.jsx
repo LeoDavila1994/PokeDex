c88ddd2c5708daf0c3e5d5b418a229c48f9ecdc3
LeoDavila1994 <liodavila.ld@gmail.com>
2022-07-23 18:19:10 -0500

    select

--- src/Components/Pokedex.jsx
+++ src/Components/Pokedex.jsx
@@ -8,18 +8,23 @@ const Pokedex = () => {
 
     const userName = useSelector(state => state.userName);
     const [pokemons, setPokemons] = useState([]);
+    const [typeModal, setTypeModal] = useState(false);
+    const [ pokemonType, setPokemonType] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("https://pokeapi.co/api/v2/pokemon/")
             .then(res => setPokemons(res.data.results))
             .catch(error => console.log(error.response))
+
+        axios.get("https://pokeapi.co/api/v2/type/")
+            .then(res => setPokemonType(res.data.results))
+            .catch(error => console.log(error.response))
     }, []);
 
-
     const [page, setPage] = useState(1);
-    const lastIndex = page * 5;
-    const firstIndex = lastIndex - 5;
+    const lastIndex = page * 10;
+    const firstIndex = lastIndex - 10;
 
     const nextPage = () => {
         setPage(page + 1);
@@ -29,31 +34,39 @@ const Pokedex = () => {
         setPage(page - 1);
     }
 
+    const filterType = e => {
+        axios.get(e.target.value)
+            .then(res => setPokemons(res.data.pokemon))
+    }
+
     const pokemonPagination = pokemons.slice(firstIndex, lastIndex);
-    const lastPage = Math.ceil(pokemons.length / 5);
+    const lastPage = Math.ceil(pokemons.length / 10);
 
     const numberPage = [];
     for (let i = 1; i <= lastPage; i++) {
         numberPage.push(i);
     }
 
-    const [ search, setSearch ] =useState("");
+    const [search, setSearch] = useState("");
 
     const submit = e => {
 
         e.preventDefault();
 
-        for (let i = 0; i < pokemons.length; i++){
-            if(search.toLowerCase() === pokemons[i].name){
+        for (let i = 0; i < pokemons.length; i++) {
+            if (search.toLowerCase() === pokemons[i].name) {
                 navigate(`/pokedex/${search.toLowerCase()}`)
                 setSearch("");
                 break;
-            } else if (pokemons[i] === pokemons[pokemons.length - 1]){
-                alert("Wrong Name");
+            } else if (pokemons[i] === pokemons[pokemons.length - 1]) {
+                setTypeModal(!typeModal);
+                setSearch("");
             }
         }
 
     }
+
+    console.log(pokemonPagination)
 
     return (
         <section>
@@ -67,26 +80,47 @@ const Pokedex = () => {
                     <p>{userName}</p>
                 </div>
             </div>
-            <div className='input-cont'>
-                <form onSubmit={submit}>
-                    <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search Pokemon by name"/>
-                    <button><i className="fa-solid fa-magnifying-glass"></i></button>
-                </form>
-            </div>
-            <div className='pokemons-container'>
-                {pokemonPagination.map(pokemon => (
-                    <PokemonCard key={pokemon.url} pokeData={pokemon.url} />
-                ))}
-                <div className='pagination-container'>
-                    <button className='poke-btn' onClick={previousPage} disabled={page === 1}><i className="fa-solid fa-angle-left"></i></button>
-                    {numberPage.map(number => (
-                        <div key={number} className="numPage" onClick={() => setPage(number)}>
-                            <p>{number}</p>
+            {typeModal ? (
+                <>
+                    <div className='type-modal-cont'>
+                        <div className='container-modal'>
+                            <div className='x' onClick={() => setTypeModal(!typeModal)}><i className="fa-regular fa-circle-xmark"></i></div>
+                            <div className='select-list'>
+                                <p>WRONG NAME!</p>
+                            </div>
                         </div>
-                    ))}
-                    <button className='poke-btn' onClick={nextPage} disabled={page === lastPage}><i className="fa-solid fa-angle-right"></i></button>
-                </div>
-            </div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <select className='selection' onChange={filterType}>
+                        <option value="">Select Type</option>
+                        {pokemonType.map(type => (
+                            <option key={type.name} value={type.url}>{type.name}</option>
+                        ))}
+                    </select>
+                    <div className='input-cont'>
+                        <form onSubmit={submit}>
+                            <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Search Pokemon by name" />
+                            <button><i className="fa-solid fa-magnifying-glass"></i></button>
+                        </form>
+                    </div>
+                    <div className='pokemons-container'>
+                        {pokemonPagination.map(pokemon => (
+                            <PokemonCard key={pokemon.url? pokemon.url : pokemon.pokemon.url} pokeData={pokemon.url? pokemon.url : pokemon.pokemon.url} />
+                        ))}
+                        <div className='pagination-container'>
+                            <button className='poke-btn' onClick={previousPage} disabled={page === 1}><i className="fa-solid fa-angle-left"></i></button>
+                            {numberPage.map(number => (
+                                <div key={number} className="numPage" onClick={() => setPage(number)}>
+                                    <p>{number}</p>
+                                </div>
+                            ))}
+                            <button className='poke-btn' onClick={nextPage} disabled={page === lastPage}><i className="fa-solid fa-angle-right"></i></button>
+                        </div>
+                    </div>
+                </>
+            )}
         </section>
     );
 };
