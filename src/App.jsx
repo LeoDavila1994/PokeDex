b42e7b0e316659b3ba9765b50d813fb4be3bb972
LeoDavila1994 <liodavila.ld@gmail.com>
2022-07-24 02:45:04 -0500

    audio

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import LogIn from './Components/LogIn';
 import Pokedex from './Components/Pokedex';
 import PokemonDetail from './Components/PokemonDetail';
 import ProtectedRoutes from './Components/ProtectedRoutes';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [soundTrack, setSoundTrack] = useState(true)
+
+  const audio = () => {
+    setSoundTrack(!soundTrack);
+  }
+
   return (
     <HashRouter>
-      <audio src="./src/Pist/PokePist.mp3" autoPlay></audio>
+      <button className='btn-sound' onClick={audio}><i className="fa-solid fa-volume-high"></i></button>
+      {soundTrack ? (<audio src="./src/Pist/PokePist.mp3" autoPlay></audio>) : <></>}
       <Routes>
         <Route path='/' element={<LogIn />} />
         <Route element={<ProtectedRoutes />}>
